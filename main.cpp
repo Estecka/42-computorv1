f32c34941ba1b4aa0a6cf6707bc2e9163108b9f8
abaur <abaur@student.42.fr>
2023-02-04 16:58:49 +0100

    Acceptable delta checker

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 17:33:32 by abaur             #+#    #+#             */
-/*   Updated: 2023/01/10 16:28:14 by abaur            ###   ########.fr       */
+/*   Updated: 2023/02/04 16:48:36 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 
 #include <iostream>
 #include <stdexcept>
+
+#define CHECK_DELTA	1e-6
 
 static float	Compute(Polynomial poly, float x){
 	return (poly[2]*x*x) + (poly[1]*x) + (poly[0]);
@@ -42,11 +44,27 @@ static int	Check(Polynomial poly, Solution sol){
 		std::cout << LOG_BOLD_GREEN "Solutions OK" LOG_CLEAR << std::endl;
 		return EXIT_SUCCESS;
 	}
-	else {
-		for (int i=0; i<2; i++)
-		if (r[i])
-			std::cout << LOG_BOLD_RED "KO: f("<<t[i]<<") = " << r[i] << LOG_CLEAR << std::endl;
-		return EXIT_FAILURE;
+	else 
+	{
+		bool ok[2] = {
+			(CHECK_DELTA >= r[0] && r[0] >= -CHECK_DELTA),
+			(CHECK_DELTA >= r[1] && r[1] >= -CHECK_DELTA),
+		};
+
+		if (ok[0] && ok[1])
+			std::cout << LOG_BOLD_GREEN "Solution acceptable" << std::endl;
+
+		for (int i=0; i<2; i++) 
+		if(r[i]) {
+			if (ok[i])
+				std::cout << LOG_BOLD_YELLOW;
+			else
+				std::cout << LOG_BOLD_RED "KO: ";
+
+			std::cout <<"f("<<t[i]<<") = " << r[i] << LOG_CLEAR << std::endl;
+		}
+
+		return (ok[0] && ok[1]) ? EXIT_SUCCESS : EXIT_FAILURE;
 	}
 }
 
