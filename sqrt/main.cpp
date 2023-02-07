0f1ef418ac28e7bfa4ec90c18adc1307b0af6c2f
abaur <abaur@student.42.fr>
2023-02-07 16:04:15 +0100

    Stats for sqrt tester

--- sqrt/main.cpp
+++ sqrt/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 14:51:51 by abaur             #+#    #+#             */
-/*   Updated: 2023/01/31 15:13:19 by abaur            ###   ########.fr       */
+/*   Updated: 2023/02/07 16:01:19 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 #include "../logutil/logutil.hpp"
 
 #include <iostream>
+#include <list>
 
 #define DELTAMAX	0.1f
 
 extern int	main(int argc, char** argv){
+	std::list<float>	results;
+
 	for (int i=1; i<argc; i++)
 	{
 		float f = atof(argv[i]);
 		float root = sqrt(f);
 		float square = root * root;
 		float delta = (square-f) / f;
+		results.push_back(delta);
 
 		const char* color;
 		if (DELTAMAX > delta && delta > -DELTAMAX)
@@ -36,4 +40,21 @@ extern int	main(int argc, char** argv){
 		          << color << delta << LOG_CLEAR
 		          << std::endl;
 	}
+
+	float deltaMax=0, deltaAvg=0;
+	for (std::list<float>::iterator it=results.begin(); it!=results.end(); ++it){
+		if (!(*it == *it) || (*it == *it*0.5)) // Exclude NaN and inf
+			continue;
+		if (*it < 0)
+			*it = -*it;
+		if (*it > deltaMax)
+			deltaMax = *it;
+		deltaAvg += *it;
+	}
+	deltaAvg /= results.size();
+	
+	std::cout << std::endl
+	          << "Average delta: " << LOG_BOLD_CLEAR << deltaAvg << LOG_CLEAR << std::endl
+	          << "Delta Max:     " << LOG_BOLD_CLEAR << deltaMax << LOG_CLEAR << std::endl
+	          ;
 }
