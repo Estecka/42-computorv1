9df613e9be380440ab880245b9fb2ddbc2f4929a
abaur <abaur@student.42.fr>
2023-02-09 18:29:12 +0100

    Tweaked sqrt

--- sqrt/sqrt.cpp
+++ sqrt/sqrt.cpp
@@ -6,16 +6,23 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 14:14:08 by abaur             #+#    #+#             */
-/*   Updated: 2023/02/09 17:26:42 by abaur            ###   ########.fr       */
+/*   Updated: 2023/02/09 18:23:23 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define SQRT_ITER	100
+#define SQRT_ITER_MAX	600
 
 extern double	sqrt(double f){
-	double root = f/10;
-	for (int i=0; i<SQRT_ITER; i++)
-		root = (root + (f/root)) * 0.5f;
+	bool ok = 0;
+	double root[2] = { (1 + f) * 0.5 };
 
-	return root;
+	for (int i=0; i<SQRT_ITER_MAX; i++) {
+		root[!ok] = (root[ok] + (f/root[ok])) * 0.5;
+		if (root[!ok] >= root[ok])
+			break;
+		else
+			ok = !ok;
+	}
+
+	return root[ok];
 }
